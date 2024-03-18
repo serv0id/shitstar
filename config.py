cbac440978f89f1fbf526dcc5cce9c27f1d69735
serv0id <113516593+serv0id@users.noreply.github.com>
2024-03-19 03:57:58 +0530

    fixed typo in user agent

--- config.py
+++ config.py
@@ -4,15 +4,15 @@ OTP_VERIFY_ENDPOINT = "https://apix.hotstar.com/v2/pages/1/spaces/1/widgets/9"
 GUEST_URL = "https://apix.hotstar.com/v2/freshstart"
 
 # Misc
-USER_AGENT = "User-Agent: Hotstar;in.startv.hotstar/23.09.11.19.9094 (Android/11)"
+USER_AGENT = "Hotstar;in.startv.hotstar/24.01.29.6.9479 (Android/13)"
 PROTOBUF_CONTENT_TYPE = "application/x-protobuf"
 
 APP_VALUE = "9479"
 APP_VALUE_ID = "79e41556-971f-439d-abee-c74fabab3f02"
 
 # edit these values to match your own if needed
-HS_CLIENT = ("X-HS-Client: platform:android;app_id:in.startv.hotstar;app_version:23.09.11.19;os:Android;os_version:11"
-             ";schema_version:0.0.1017;brand:Xiaomi;model:Redmi Note 6 Pro;carrier:airtel;network_data:UNKNOWN")
+HS_CLIENT = ("X-HS-Client: platform:android;app_id:in.startv.hotstar;app_version:24.01.29.6;os:Android;os_version:13;"
+             "schema_version:0.0.1133;brand:Xiaomi;model:Redmi Note 12 4G;carrier:airtel;network_data:NETWORK_TYPE_WIFI")
 HS_CLIENT_TARGETING = "ad_id:d5b2ce8c-7552-433e-9aac-ad9503a1c496;user_lat:false;hw_id:43fdd27a0f0eb904"
 HS_PLATFORM = "android"
 HS_SCHEMA_VERSION = "0.0.1133"
