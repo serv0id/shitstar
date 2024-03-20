77138edc000c565813a11847c354b2b0a1f46c14
serv0id <113516593+serv0id@users.noreply.github.com>
2024-03-20 11:28:16 +0530

    fixed guest user token retrieval

--- config.py
+++ config.py
@@ -16,6 +16,23 @@ HS_CLIENT = ("X-HS-Client: platform:android;app_id:in.startv.hotstar;app_version
 HS_CLIENT_TARGETING = "ad_id:d5b2ce8c-7552-433e-9aac-ad9503a1c496;user_lat:false;hw_id:43fdd27a0f0eb904"
 HS_PLATFORM = "android"
 HS_SCHEMA_VERSION = "0.0.1133"
+FP_SAMPLE_VALUE = ('MDA3MGYyZTAtZGYxMS00ODhjLThlNmItYjczYmEwNjNhYjIz.BPicufgJ44ZZHXqTv8pNoJXgX2WYShUBEW8vws'
+                   '__IjBu1VBsW5t32-Q0A8EFjVP0Wl7fvAEIDtfDMTqUQHJ5bNIRd9uO6-6mviC7-Axb9ZSwD3VCAclSrxGotyIgc'
+                   'axpXZSC9w6rijZGqbp8Hr3FkiHZTG6fqCdlVifI0ONKxowYqWKwfL9PqzngxBW4IGLm6k__sMgoPTDTEdUJDM3A'
+                   'gsFd2RIdw4WpU8ydA1OnXiyjlrqIJvNQ0riuqrLILC4UQ4j3oU_-yNwQPO1NRChLMCiQzLsG8Gr35oMPhcxoKur'
+                   '0Rv3M7oJR-PaFVrtwZhnreWtZ3Yyj5ySkkhFFh7qHENQRRj-paiWnaNny4BLhlcWPji1Lb6sZLTdjQAEvXTL38K'
+                   'MiFBcgxkaVgRAFhCiuTVqx4LPQ3oicviTI5LdocPAfGHunCSPwi-nnML_hEAXRlw3GXGZcsmujLeMgrJVwyn05y'
+                   'NJC6RXPWRuYpv4Z00XI7Arm9Ze3mczscGj6EEapvocDMaJ22lHs-_YXBDEFVYti-wxbi6Pm94xksOc5lK7M2ak3'
+                   'tjtYMS1bGr1u94sw8eo4iw260Nd7pffC9-g2S2V_otN6Ep1lDudRkFciZf5V6h3GVsyOpY78QYcNRhSfUUxVN1W'
+                   'IKwzuV_xjKKc2P8H_LRUle51dcxQ0J0cDY1J56vu7xgIkRQOBTDJs7mGP4WpVZh3A7xftZADA2nGIW9kFWiMDru'
+                   'Cw7lUVv-5DPV62fOvvdvNA7ovAxhr-ulhcR3T-BdPp3aLsuG2w23ZeL1S2va6pX_XYnViTiqRQzkXlCTQf4EhW7'
+                   'EFxKJBAOQs0V9Rv6Ffp-mdsxvb1U1YvBFaMmHwp-XMBWilf2v6DD63RsmFWeIjGg0EqG8vwlIeMYXqs2b4jFDy-'
+                   '6LMWH1YTsqrBRWVb-Uw7dQtU57lDhu4xeOAUyb78h_T8C4g4m6SNv0LnT4zAvoN1OqH_my7pmD6PQzWy8lNGBAz'
+                   '3MdvcZGHj5JUemyngYSPWF33lyGrRfpC_WFSeqMYbLZ8-46gjjQQPKHS0igkbLs-O8oknkROq_rsQwgas75kXKA'
+                   'Eydl0xSdOw3fcPuT7hPIk0N1OwyFSqkQbrqrwYFZHANLp6tebU6vOqgPmc265o-X1NOfxeiQidmS27pr4DIFiAN'
+                   'hn1zPmx3JF4YgIHWeSOFYdlw05ashCCDtDO-vqsUiU-4cNnyVgex9lL6ed8JucOjLlycQW6TOftwElBbNdZ_sT6'
+                   'ydcs50L2sr5FkOoLA1Gnj4hitW5LZQUJpY7YqzYllFi5z99711aAfHcLH3M7EdCo0GaM')
+
 
 CLIENT_CAPABILITIES = {"package": ["dash", "hls"], "container": ["fmp4", "fmp4br", "ts"],
                        "ads": ["non_ssai", "ssai", "sgai"], "audio_channel": ["stereo"],
--- helpers/utils.py
+++ helpers/utils.py
@@ -65,8 +65,7 @@ def get_guest_token(session: requests.Session, device_id: str) -> str:
         "subs": "null",
         "login": "UNKNOWN"
     }, headers={
-        "X-HS-FP-Info": "MDA3MGYyZTAtZGYxMS00ODhjLThlNmItYjczYmEwNjNhYjIz.BPicufgJ44ZZHXqTv8pNoJXgX2WYShUBEW8vws__IjBu1VBsW5t32-Q0A8EFjVP0Wl7fvAEIDtfDMTqUQHJ5bNIRd9uO6-6mviC7-Axb9ZSwD3VCAclSrxGotyIgcaxpXZSC9w6rijZGqbp8Hr3FkiHZTG6fqCdlVifI0ONKxowYqWKwfL9PqzngxBW4IGLm6k__sMgoPTDTEdUJDM3AgsFd2RIdw4WpU8ydA1OnXiyjlrqIJvNQ0riuqrLILC4UQ4j3oU_-yNwQPO1NRChLMCiQzLsG8Gr35oMPhcxoKur0Rv3M7oJR-PaFVrtwZhnreWtZ3Yyj5ySkkhFFh7qHENQRRj-paiWnaNny4BLhlcWPji1Lb6sZLTdjQAEvXTL38KMiFBcgxkaVgRAFhCiuTVqx4LPQ3oicviTI5LdocPAfGHunCSPwi-nnML_hEAXRlw3GXGZcsmujLeMgrJVwyn05yNJC6RXPWRuYpv4Z00XI7Arm9Ze3mczscGj6EEapvocDMaJ22lHs-_YXBDEFVYti-wxbi6Pm94xksOc5lK7M2ak3tjtYMS1bGr1u94sw8eo4iw260Nd7pffC9-g2S2V_otN6Ep1lDudRkFciZf5V6h3GVsyOpY78QYcNRhSfUUxVN1WIKwzuV_xjKKc2P8H_LRUle51dcxQ0J0cDY1J56vu7xgIkRQOBTDJs7mGP4WpVZh3A7xftZADA2nGIW9kFWiMDruCw7lUVv-5DPV62fOvvdvNA7ovAxhr-ulhcR3T-BdPp3aLsuG2w23ZeL1S2va6pX_XYnViTiqRQzkXlCTQf4EhW7EFxKJBAOQs0V9Rv6Ffp-mdsxvb1U1YvBFaMmHwp-XMBWilf2v6DD63RsmFWeIjGg0EqG8vwlIeMYXqs2b4jFDy-6LMWH1YTsqrBRWVb-Uw7dQtU57lDhu4xeOAUyb78h_T8C4g4m6SNv0LnT4zAvoN1OqH_my7pmD6PQzWy8lNGBAz3MdvcZGHj5JUemyngYSPWF33lyGrRfpC_WFSeqMYbLZ8-46gjjQQPKHS0igkbLs-O8oknkROq_rsQwgas75kXKAEydl0xSdOw3fcPuT7hPIk0N1OwyFSqkQbrqrwYFZHANLp6tebU6vOqgPmc265o-X1NOfxeiQidmS27pr4DIFiANhn1zPmx3JF4YgIHWeSOFYdlw05ashCCDtDO-vqsUiU-4cNnyVgex9lL6ed8JucOjLlycQW6TOftwElBbNdZ_sT6ydcs50L2sr5FkOoLA1Gnj4hitW5LZQUJpY7YqzYllFi5z99711aAfHcLH3M7EdCo0GaM",
-        "X-HS-Request-Id": "288fb503-18aa-43ab-9fd8-a2ab69e98cb3",
+        "X-HS-FP-Info": config.FP_SAMPLE_VALUE,
         'hotstarauth': get_hs_auth()
     }, data=ProtoHelper.get_freshstart(device_id))
 
